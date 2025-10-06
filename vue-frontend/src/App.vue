b0f09277ffd2b84645f0ee4dce9c70615d23661e
Artem <luostra@yandex.ru>
2025-10-06 16:10:26 +0300

    Update App.vue

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -1,85 +1,96 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { Button, Menubar } from 'primevue'
+import { ref } from 'vue'
+import { useAuthStore } from '@/stores/auth'
+import { storeToRefs } from 'pinia'
+import { useThemeStore } from '@/stores/useTheme'
+import { onMounted } from 'vue'
+
+const authStore = useAuthStore()
+
+onMounted(() => {
+  authStore.checkAuth()
+})
+const themeStore = useThemeStore()
+themeStore.initTheme()
+//    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+const items = ref([
+  {
+    label: 'Задачи',
+    icon: 'pi pi-pen-to-square',
+    path: '/',
+  },
+  {
+    label: 'О программе',
+    icon: 'pi pi-info',
+    path: '/about',
+  },
+  {
+    label: 'Войти',
+    icon: 'pi pi-user',
+    path: '/login',
+  },
+])
+
+const { logout } = authStore
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+  <Menubar
+    :model="items"
+    class="!fixed !backdrop-blur-sm !bg-emerald-500/40 w-full h-auto z-10000 !border-0 dark:!bg-emerald-500/40"
+    :pt="{
+      root: { class: '!flex !justify-between !gap-4' },
+      rootList: { class: 'order-2 !ml-auto' },
+      itemContent: { class: '' },
+      button: {
+        class: 'order-2 !ml-auto !text-white hover:!bg-emerald-600 active:!bg-emerald-700',
+      },
+    }"
+  >
+    <template #start>
+      <Button
+        @click="themeStore.toggleTheme"
+        :icon="themeStore.isDarkTheme ? 'pi pi-moon' : 'pi pi-sun'"
+        class="ml-2 p-button-text !text-gray-700 dark:!text-white"
+        text
+      />
+    </template>
+    <template #item="{ item, props }">
+      <a class="flex items-center" v-bind="props.action">
+        <RouterLink :to="item.path" v-if="authStore.isAuthenticated && item.label != 'Войти'">
+          <span :class="item.icon" />
+          <span class="pl-1">{{ item.label }}</span>
+        </RouterLink>
+        <RouterLink :to="item.path" v-if="!authStore.isAuthenticated && item.label == 'Войти'">
+          <span :class="item.icon" />
+          <span class="pl-1">{{ item.label }}</span>
+        </RouterLink>
+        <RouterLink
+          :to="item.path"
+          v-if="!authStore.isAuthenticated && item.label == 'О программе'"
+        >
+          <span :class="item.icon" />
+          <span class="pl-1">{{ item.label }}</span>
+        </RouterLink>
+        <RouterLink to="/login">
+          <Button
+            unstyled=""
+            label="  Выйти"
+            @click="logout"
+            text
+            icon="pi pi-sign-out"
+            severity="danger"
+            v-if="authStore.isAuthenticated && item.label == 'Войти'"
+          ></Button>
+        </RouterLink>
+      </a>
+    </template>
+    <template #end> </template>
+  </Menubar>
 
   <RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style scoped></style>
