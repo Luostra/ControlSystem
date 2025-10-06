81c74aaaa2dd57b1601a25dba94798c281201163
Artem <luostra@yandex.ru>
2025-10-06 13:24:02 +0300

    config files edited

--- vue-frontend/src/main.js
+++ vue-frontend/src/main.js
@@ -1,11 +1,33 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
 import App from './App.vue'
 import router from './router'
 
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+import AnimateOnScroll from 'primevue/animateonscroll'
+import ToastService from 'primevue/toastservice'
+
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
 const app = createApp(App)
 
+app.use(pinia)
 app.use(router)
+app.use(ToastService)
 
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: '.my-app-dark',
+    },
+  },
+})
+app.directive('animateonscroll', AnimateOnScroll)
 app.mount('#app')
