d9c9a780a74707698e27d8a0a5d26777857c9bdd
Artem <luostra@yandex.ru>
2025-10-06 14:46:54 +0300

    init config of the router

--- vue-frontend/src/router/index.js
+++ vue-frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import { useAuthStore } from '@/stores/auth'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,7 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+      meta: { requiresAuth: true }
     },
     {
       path: '/about',
@@ -17,7 +19,35 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/login',
+      name: 'log in',
+      component: () => import('../views/LogInView.vue'),
+      meta: { requiresGuest: true },
+    },
+    {
+      path: '/signUp',
+      name: 'sign up',
+      component: () => import('../views/SignUpView.vue'),
+      meta: { requiresGuest: true },
+    },
   ],
 })
 
+router.beforeEach(async (to, from, next) => {
+  const authStore = useAuthStore()
+  
+  // Проверяем аутентификацию при первом посещении
+  if (!authStore.user && authStore.token) {
+    await authStore.checkAuth()
+  }
+
+  if (to.meta.requiresAuth && !authStore.isAuthenticated) {
+    next('/login')
+  } else if (to.meta.requiresGuest && authStore.isAuthenticated) {
+    next('/')
+  } else {
+    next()
+  } 
+})
 export default router
